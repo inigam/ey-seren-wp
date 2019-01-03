50aff63e2242014628bf94a468003ca1dd03a811
Ikshit <ikshit.nigam@gmail.com>
2019-01-03 22:02:11 +0000

    Update and rename wp-config-sample.php to wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'database_name_here');
+define('DB_NAME', 'eys02');
 
 /** MySQL database username */
-define('DB_USER', 'username_here');
+define('DB_USER', 'eyseren02');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'password_here');
+define('DB_PASSWORD', 'eyseren02');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
